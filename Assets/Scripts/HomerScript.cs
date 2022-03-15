8de5561bb79194b467204500499f349a40c68fff
M-Unterreiner <martin.unterreiner@posteo.de>
2022-03-15 14:37:01 +0100

    Implemented HandCollider

--- Assets/Scripts/HomerScript.cs
+++ Assets/Scripts/HomerScript.cs
@@ -15,12 +15,34 @@ public class HomerScript : MonoBehaviour
     private RayScript mySelectionRay;
     private GameObject lastSelectedObject = null;
 
-   
+    private GameObject hand;
+    private GameObject handController;
+    private GameObject handCenter;
+    private XRController handXRController;
+
+    private CollisionDetectorScript handDetector;
+    private GameObject handColliderProxy;
+
 
     void Awake()
     {
         mySelectionRay = GetComponent<RayScript>();
-        mySelectionRay.enableRay();        
+        mySelectionRay.enableRay();
+
+        handController = GameObject.Find("RightHand Controller");
+        handXRController = handController.GetComponent<XRController>();
+        hand = transform.Find("Camera Offset/RightHand Controller/HandRight").gameObject;
+        handCenter = transform.Find("Camera Offset/RightHand Controller/RightHandCenter").gameObject;
+
+        handColliderProxy = GameObject.Find("HandColliderProxy");
+        handDetector = handColliderProxy.GetComponent<CollisionDetectorScript>();
+    }
+
+    private void Update()
+    {
+        // Debug.Log("Does hand collide: " + handDetector.collided);
+        Debug.Log("Position hand: " + hand.transform.position + "Collider "+ handDetector.transform.position);
+        checkHandCollition();
     }
 
     private void OnDisable()
@@ -53,8 +75,7 @@ public class HomerScript : MonoBehaviour
 
     public void selectByHitRay(GameObject HandController)
     {
-        Debug.Log(mySelectionRay.showRay(HandController));
-
+        //Debug.Log(mySelectionRay.showRay(HandController));
         lastSelectedObject = mySelectionRay.showRay(HandController);
     }
 
@@ -63,12 +84,28 @@ public class HomerScript : MonoBehaviour
         if(lastSelectedObject != null)
         {
             Debug.Log("Grab Object: " + lastSelectedObject);
+            if (!handDetector.collided)
+            {
+                moveHandToObject(hand, handCenter);
+            }
+
         }
+    }
+
+    public void checkHandCollition()
+    {
+        handColliderProxy.transform.localPosition = hand.transform.localPosition;
     }
 
     public void deGrabHomer()
     {
-        Debug.Log("deGrabbing not yet implemented");
+        // Set Hand back to position of Controller
+        hand.transform.position = handController.transform.position;
+    }
+
+    public void moveHandToObject(GameObject hand, GameObject handCenter)
+    {
+        hand.transform.position = Vector3.MoveTowards(hand.transform.position, lastSelectedObject.transform.position, 5.0f * Time.deltaTime);
     }
 
 
