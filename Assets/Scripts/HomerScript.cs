d81287ca96ae7d435e7fb53868edabbfa6cf02f1
M-Unterreiner <martin.unterreiner@posteo.de>
2022-03-16 15:53:08 +0100

    Added  setGrabbedObjectHandPosition

--- Assets/Scripts/HomerScript.cs
+++ Assets/Scripts/HomerScript.cs
@@ -130,8 +130,13 @@ public class HomerScript : MonoBehaviour
     */
     private void grabObject(GameObject collidedObject)
     {
-        //setGrabbedObjectHandPosition(collidedObject.transform.position);
+        setGrabbedObjectHandPosition(collidedObject.transform.position);
         //changeParentOfHand(grabbedObjectHandPosition);
         //Debug.Log("New Parent of hand: " + hand.transform.parent.name);
+    }
+
+    private void setGrabbedObjectHandPosition(Vector3 newHandPosition)
+    {
+        grabbedObjectHandPosition.transform.position = newHandPosition;
     }
 }
