d0d429b149be81c3c17ca2d001e13e3ea578db9f
M-Unterreiner <martin.unterreiner@posteo.de>
2022-03-17 17:42:14 +0100

    Deactivate deGrabHomer

--- Assets/Scripts/ControllerScript.cs
+++ Assets/Scripts/ControllerScript.cs
@@ -110,7 +110,7 @@ public class ControllerScript : MonoBehaviour
             if (gogo.isActiveAndEnabled) gogo.stopGoGoHand();
             if (gogoFast.isActiveAndEnabled) gogoFast.stopGoGoHand();
             if (gogoStrech.isActiveAndEnabled) gogoStrech.stopGoGoHand();
-            if (homer.isActiveAndEnabled) homer.deGrabHomer();
+            //if (homer.isActiveAndEnabled) homer.deGrabHomer();
         }      
     }
 
